a3e2751c64632cda54559ba9a56df4f07094fcd7
kendra-svg <ksanchov@ucenfotec.ac.cr>
2025-04-28 21:55:31 -0600

    Add unpractical approach by removing duplicates on a sorted array by using lists example

--- dsa-alg/DuplicatesOnSortedArray.cs
+++ dsa-alg/DuplicatesOnSortedArray.cs
@@ -33,7 +33,30 @@ namespace dsa_alg
             return result;
         }
 
-        
+        public int[] RemoveUsingLists() //Overall time complexity O(n)
+        {
+            int[] A = { 1, 1, 1, 1, 1, 3, 4, 6, 7 };
+            List<int> list = new List<int>(A); //Array is tuened into list
+
+            List<int> finalList = new List<int>(); //It will store unique elements
+            HashSet<int> seen = new HashSet<int>(); //To check if we already saw the number
+
+            foreach (int num in list)
+            {
+                if (!seen.Contains(num))
+                {
+                    seen.Add(num);
+                    finalList.Add(num);
+                }
+            }
+            return finalList.ToArray();
+
+            //This is not the best solution because I started with an array, and immediately after I copied it into a list (duplicating memory), then I created two more structures (seen HashSet) and (finalList list). 
+
+            //Extra transformations: I turned it into an array, then into a list, then into an array again, which is wasted effort.
+
+            //Long story short, I was doing extra copying, extra memory usage, and extra transformations that could be avoided by either working direclty on the array or by using onlu a hashset and a new List without copying the original array into a list.
+        }
 
 
     }
